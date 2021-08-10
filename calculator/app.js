b6b9d08992bc18bb41e0b43d7d772510bc7a553e
alexandru miroiu <55791919+alexmru@users.noreply.github.com>
2021-08-10 22:38:22 +0300

    adaugare functionalitate , testare adunare

--- calculator/app.js
+++ calculator/app.js
@@ -11,24 +11,55 @@ const equalsBtn = document.querySelector('.equals');
 const numberButtons = document.querySelectorAll('.number');
 
 class Calculator {
+    display;
+    currentOperation;
+    currentValue = '0';
+    storedValue;
+    result;
+
 
     constructor(display) {
         this.display = display; 
     }
 
     addNumber(number) {
-        this.currentNumber = number;
+        if(number === '.' && this.currentValue.includes('.')){
+            return;
+        }
+        if(this.currentValue.charAt(0) === '0') {
+        this.currentValue = this.currentValue.replace('0', '');
+        }
+        this.currentValue += number;
+        console.log(this.currentValue.charAt(0));
     }
 
     show() {
-        this.display.innerText += this.currentNumber;
+        this.display.innerText = this.currentValue.toString();
+    }
+
+    store() {
+        this.storedValue = this.currentValue;
+        this.currentValue = '';
     }
 
     clear() {
         this.display.innerText = '';
-        this.currentNumber = '0';
+        this.currentValue = '0';
+        this.currentOperation = '';
+    }
 
-        // this.currentOperation = '';
+    setOperation(operation) {
+        if(operation === '+') {
+        this.currentOperation = 'addition';
+        }
+    }
+
+    compute() {
+        
+        if(this.currentOperation === 'addition') {
+            let secondValue = Number(this.currentValue) ;
+            this.currentValue = Number(this.storedValue) + secondValue;
+        }
     }
 }
 
@@ -37,6 +68,7 @@ const calculator = new Calculator(displayArea);
 
 numberButtons.forEach(button => {
     button.addEventListener('click', () => {
+        calculator.display.innerText = '';
         calculator.addNumber(button.innerText);
         calculator.show();
         console.log(button.innerText)
@@ -48,3 +80,12 @@ allClearBtn.addEventListener('click', () => {
     calculator.show();
 });
 
+addBtn.addEventListener('click', () => {
+    calculator.store();
+    calculator.setOperation(addBtn.innerText);
+})
+
+equalsBtn.addEventListener('click', () => {
+    calculator.compute();
+    calculator.show();
+})
