ea0f54ef38a70f55c7bd7b2325e32a4dcf7cab27
alexandru miroiu <55791919+alexmru@users.noreply.github.com>
2021-08-11 22:03:26 +0300

    adaugare functionalitate pentru scadere, inmultire, impartire

--- calculator/app.js
+++ calculator/app.js
@@ -39,8 +39,10 @@ class Calculator {
     }
 
     store() {
+        console.log(this.currentValue)
         this.storedValue = this.currentValue;
         this.currentValue = '';
+        this.targetButton= '';
     }
 
     clear() {
@@ -62,17 +64,57 @@ class Calculator {
         if(operation === '+') {
         this.currentOperation = 'addition';
         }
+        if(operation === '-') {
+        this.currentOperation = 'subtraction';
+        }
+        if(operation === 'x') {
+        this.currentOperation = 'multiplication';
+        console.log('changed to multiplication')
+        }
+        if(operation === '÷') {
+        this.currentOperation = 'division';
+        console.log('changed to division')
+        }
+
     }
 
     compute(target) {
-        console.log(this.targetButton);
+        // console.log(this.targetButton);
         if(this.currentOperation === 'addition') {
+            console.log('addition')
             if(this.targetButton === target) {
                 this.currentValue = Number(this.currentValue) + Number(this.repeatValue);
                 return;
             }
             this.repeatValue = this.currentValue;
             this.currentValue = Number(this.storedValue) + Number(this.currentValue)
+        }
+        if(this.currentOperation === 'subtraction') {
+            if(this.targetButton === target) {
+                console.log('bang')
+                this.currentValue = Number(this.currentValue) - Number(this.repeatValue);
+                return;
+            }
+            this.repeatValue = this.currentValue;
+            this.currentValue = Number(this.storedValue) - Number(this.currentValue)
+        }
+        if(this.currentOperation === 'division') {
+            console.log('division')
+            if(this.targetButton === target) {
+                this.currentValue = Number(this.currentValue) / Number(this.repeatValue);
+                return;
+            }
+            this.repeatValue = this.currentValue;
+            this.currentValue = Number(this.storedValue) / Number(this.currentValue)
+        }
+        if(this.currentOperation === 'multiplication') {
+            console.log('multiplication')
+            if(this.targetButton === target) {
+                this.currentValue = Number(this.currentValue) * Number(this.repeatValue);
+                return;
+            }
+            this.repeatValue = this.currentValue;
+            this.currentValue = Number(this.storedValue) * Number(this.currentValue)
         }
     }
 }
@@ -85,7 +127,6 @@ numberButtons.forEach(button => {
         calculator.display.innerText = '';
         calculator.addNumber(button.innerText);
         calculator.show();
-        console.log(button.innerText)
     })
 });
 
@@ -99,6 +140,25 @@ addBtn.addEventListener('click', () => {
     calculator.setOperation(addBtn.innerText);
     calculator.show();
 })
+subtractBtn.addEventListener('click', () => {
+    calculator.store();
+    calculator.setOperation(subtractBtn.innerText);
+    calculator.show();
+})
+multiplyBtn.addEventListener('click', () => {
+    calculator.store();
+    calculator.setOperation(multiplyBtn.innerText);
+    calculator.show();
+})
+divideBtn.addEventListener('click', () => {
+    calculator.store();
+    calculator.setOperation(divideBtn.innerText);
+    console.log(divideBtn.innerText)
+    calculator.show();
+    
+})
+
+
 
 equalsBtn.addEventListener('click', (e) => {
     calculator.compute(e.target.innerText);
