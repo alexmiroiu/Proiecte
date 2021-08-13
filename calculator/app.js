24b0ac1bae0197f3ff3cc1507015defa89b246db
alexandru miroiu <55791919+alexmru@users.noreply.github.com>
2021-08-13 09:24:29 +0300

    proiect terminat

--- calculator/app.js
+++ calculator/app.js
@@ -20,39 +20,57 @@ class Calculator {
 
 
     constructor(display) {
-        this.display = display; 
+        this.display = display;
     }
 
     addNumber(number) {
-        if(number === '.' && this.currentValue.includes('.')){
+        if (number === '.' && this.currentValue.includes('.')) {
             return;
         }
-        if(this.currentValue.charAt(0) === '0') {
-        this.currentValue = this.currentValue.replace('0', '');
+        if (this.currentValue.charAt(0) === '0') {
+            this.currentValue = this.currentValue.replace('0', '');
         }
         this.currentValue += number;
     }
 
     show() {
         this.display.innerText = this.currentValue.toString();
-        if(this.currentValue.length < 7) {
+        if (this.currentValue.length < 7) {
             this.display.style.fontSize = '9rem';
         }
-        if(this.currentValue.length == 7 ) {
-            this.display.style.fontSize = '8rem';
+        if (this.currentValue.length > 13) {
+            this.display.style.fontSize = '2.7rem';
         }
-        if(this.currentValue.length == 8) {
-            this.display.style.fontSize = '7rem';
-        }
-        if(this.currentValue.length == 9) {
-            this.display.style.fontSize = '6.5rem';
+        switch (this.currentValue.length) {
+            case 7:
+                this.display.style.fontSize = '8rem';
+                break;
+            case 8:
+                this.display.style.fontSize = '7rem';
+                break;
+            case 9:
+                this.display.style.fontSize = '6.5rem';
+                break;
+            case 10:
+                this.display.style.fontSize = '6rem';
+                break;
+            case 11:
+                this.display.style.fontSize = '5.5rem';
+                break;
+            case 12:
+                this.display.style.fontSize = '4.5rem';
+                break;
+            case 13:
+                this.display.style.fontSize = '3.5rem';
+                break;
+
         }
     }
 
     store() {
         this.storedValue = this.currentValue;
         this.currentValue = '';
-        this.targetButton= '';
+        this.targetButton = '';
     }
 
     clear() {
@@ -64,26 +82,26 @@ class Calculator {
         this.repeatValue = '';
     }
     getTarget(target) {
-        if(this.targetButton === target) {
+        if (this.targetButton === target) {
             return;
         }
         this.targetButton = target;
     }
 
     setOperation(operation) {
-        if(operation === '+') {
-        this.currentOperation = 'addition';
+        if (operation === '+') {
+            this.currentOperation = 'addition';
         }
-        if(operation === '-') {
-        this.currentOperation = 'subtraction';
+        if (operation === '-') {
+            this.currentOperation = 'subtraction';
         }
-        if(operation === 'x') {
-        this.currentOperation = 'multiplication';
-        console.log('changed to multiplication')
+        if (operation === 'x') {
+            this.currentOperation = 'multiplication';
+            console.log('changed to multiplication')
         }
-        if(operation === '÷') {
-        this.currentOperation = 'division';
-        console.log('changed to division')
+        if (operation === '÷') {
+            this.currentOperation = 'division';
+            console.log('changed to division')
         }
 
     }
@@ -93,27 +111,27 @@ class Calculator {
     }
 
     negPos() {
-        if(this.currentValue > 0 ) {
+        if (this.currentValue > 0) {
             this.currentValue = (0 - Number(this.currentValue)).toString();
-            
-        } else if(this.currentValue < 0 ) {
+
+        } else if (this.currentValue < 0) {
             this.currentValue = (Number(this.currentValue) * -1).toString();
         }
     }
 
     compute(target) {
         // console.log(this.targetButton);
-        if(this.currentOperation === 'addition') {
+        if (this.currentOperation === 'addition') {
             console.log('addition')
-            if(this.targetButton === target) {
+            if (this.targetButton === target) {
                 this.currentValue = (Number(this.currentValue) + Number(this.repeatValue)).toString();
                 return;
             }
             this.repeatValue = this.currentValue;
             this.currentValue = (Number(this.storedValue) + Number(this.currentValue)).toString();
         }
-        if(this.currentOperation === 'subtraction') {
-            if(this.targetButton === target) {
+        if (this.currentOperation === 'subtraction') {
+            if (this.targetButton === target) {
                 console.log('bang')
                 this.currentValue = (Number(this.currentValue) - Number(this.repeatValue)).toString();
                 return;
@@ -121,18 +139,18 @@ class Calculator {
             this.repeatValue = this.currentValue;
             this.currentValue = (Number(this.storedValue) - Number(this.currentValue)).toString();
         }
-        if(this.currentOperation === 'division') {
+        if (this.currentOperation === 'division') {
             console.log('division')
-            if(this.targetButton === target) {
+            if (this.targetButton === target) {
                 this.currentValue = (Number(this.currentValue) / Number(this.repeatValue)).toString();
                 return;
             }
             this.repeatValue = this.currentValue;
             this.currentValue = (Number(this.storedValue) / Number(this.currentValue)).toString();
         }
-        if(this.currentOperation === 'multiplication') {
+        if (this.currentOperation === 'multiplication') {
             console.log('multiplication')
-            if(this.targetButton === target) {
+            if (this.targetButton === target) {
                 this.currentValue = (Number(this.currentValue) * Number(this.repeatValue)).toString();
                 return;
             }
@@ -163,34 +181,36 @@ switchNegPosBtn.addEventListener('click', () => {
     calculator.show();
 })
 
-
 addBtn.addEventListener('click', () => {
     calculator.store();
     calculator.setOperation(addBtn.innerText);
     calculator.show();
 })
+
 subtractBtn.addEventListener('click', () => {
     calculator.store();
     calculator.setOperation(subtractBtn.innerText);
     calculator.show();
 })
+
 multiplyBtn.addEventListener('click', () => {
     calculator.store();
     calculator.setOperation(multiplyBtn.innerText);
     calculator.show();
 })
+
 divideBtn.addEventListener('click', () => {
     calculator.store();
     calculator.setOperation(divideBtn.innerText);
     console.log(divideBtn.innerText)
     calculator.show();
-    
+
 })
+
 percentageBtn.addEventListener('click', () => {
     calculator.percent();
     calculator.show();
 })
-
 
 equalsBtn.addEventListener('click', (e) => {
     calculator.compute(e.target.innerText);
