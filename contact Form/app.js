a83b4f5f515f3dd9ea7d6d8e75e10f5dfd45f2eb
alexandru miroiu <55791919+alexmru@users.noreply.github.com>
2021-08-23 09:26:36 +0300

    implementare validare pentru primele 3 campuri

--- contact Form/app.js
+++ contact Form/app.js
@@ -15,14 +15,32 @@ class FormHandler {
         this.message = msg;
     }
 
-    validateFirstName () {
-        const regEx = /^[\w'\-,.]*[^_!¡?÷?¿\/\\+=@#$%ˆ&*(){}|~<>;:[\]]*$/i;
-        if(regEx.test(this.firstName.value)) {
-            this.firstNameValidator = true;
-        } else {
-            this.firstNameValidator = false;
+    validateName (event) {
+        const regExName = /^[\w'\-,.]*[^_!¡?÷?¿\/\\+=@#$%ˆ&*(){}|~<>;:[\]]*$/i;
+        if(event.target.name === 'fname') {
+            if(regExName.test(this.firstName.value) && this.firstName.value.length > 1 && this.firstName.value.length < 40) {
+                this.firstNameValidator = true;
+            } else {
+                this.firstNameValidator = false;
+            }
         }
+        if(event.target.name === 'lname') {
+            if(regExName.test(this.lastName.value && this.lastName.value.length > 1) && this.lastName.value.length < 40) {
+                this.lastNameValidator = true;
+            } else {
+                this.lastNameValidator = false;
+            }
+        }
+        if(event.target.name === 'username') {
+            if(regExName.test(this.username.value) && this.username.value.length > 3 && this.username.value.length < 25) {
+                this.userNameValidator = true;
+            } else {
+                this.userNameValidator = false;
+            }
+        }
+
     }
+
 }
 
 
@@ -33,11 +51,38 @@ const password = document.getElementById('password');
 const email = document.getElementById('email');
 const message = document.getElementById('message');
 
+const errorColor = '#FF6B6B';
+
 const formHandler = new FormHandler(firstName, lastName, userName, password, email, message);
-document.addEventListener('input', () => {
-    formHandler.validateFirstName();
+
+
+firstName.addEventListener('input', (e) => {
+    formHandler.validateName(e);
     if(!formHandler.firstNameValidator) {
-        firstName.classList.add('invalid');
+        firstName.style.color = errorColor;
+    }
+    if(formHandler.firstNameValidator) {
+        firstName.style.color = 'var(--text)';
+    }
+})
+
+lastName.addEventListener('input', (e) => {
+    formHandler.validateName(e);
+    if(!formHandler.lastNameValidator) {
+        lastName.style.color = errorColor;
+    }
+    if(formHandler.lastNameValidator) {
+        lastName.style.color = 'var(--text)';
+    }
+})
+
+userName.addEventListener('input', (e) => {
+    formHandler.validateName(e);
+    if(!formHandler.userNameValidator) {
+        userName.style.color = errorColor;
+    }
+    if(formHandler.userNameValidator) {
+        userName.style.color = 'var(--text)';
     }
 })
 
