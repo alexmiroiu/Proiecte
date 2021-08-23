3b4f4591d4f7714ad80472279b24eb3879636d1c
alexandru miroiu <55791919+alexmru@users.noreply.github.com>
2021-08-23 22:53:42 +0300

    am terminat de validat campurile, urmeaza adaugarea feedback-ului

--- contact Form/app.js
+++ contact Form/app.js
@@ -6,39 +6,65 @@ class FormHandler {
     emailValidator = false;
 
 
-    constructor(fname, lname, username, pw, mail, msg) {
+    constructor(fname, lname, username, pw, mail) {
         this.firstName = fname;
         this.lastName = lname;
         this.username = username;
         this.password = pw;
         this.email = mail;
-        this.message = msg;
     }
 
-    validateName (event) {
+    validateName(event) {
         const regExName = /^[\w'\-,.]*[^_!¡?÷?¿\/\\+=@#$%ˆ&*(){}|~<>;:[\]]*$/i;
-        if(event.target.name === 'fname') {
-            if(regExName.test(this.firstName.value) && this.firstName.value.length > 1 && this.firstName.value.length < 40) {
+        const regExPass = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/;
+        const regExMail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+        if (event.target.name === 'fname') {
+            if (regExName.test(this.firstName.value) && this.firstName.value.length > 1 && this.firstName.value.length < 40) {
                 this.firstNameValidator = true;
             } else {
                 this.firstNameValidator = false;
             }
         }
-        if(event.target.name === 'lname') {
-            if(regExName.test(this.lastName.value && this.lastName.value.length > 1) && this.lastName.value.length < 40) {
+        if (event.target.name === 'lname') {
+            if (regExName.test(this.lastName.value) && this.lastName.value.length > 1 && this.lastName.value.length < 40) {
                 this.lastNameValidator = true;
             } else {
                 this.lastNameValidator = false;
             }
         }
-        if(event.target.name === 'username') {
-            if(regExName.test(this.username.value) && this.username.value.length > 3 && this.username.value.length < 25) {
+        if (event.target.name === 'username') {
+            if (regExName.test(this.username.value) && this.username.value.length > 3 && this.username.value.length < 25) {
                 this.userNameValidator = true;
             } else {
                 this.userNameValidator = false;
             }
         }
+        if (event.target.name === 'password') {
+            if (regExPass.test(this.password.value) && this.password.value.length < 40) {
+                this.passwordValidator = true;
+            } else {
+                this.passwordValidator = false;
+            }
+        }
+        if (event.target.name === "email") {
+            if (regExMail.test(this.email.value)) {
+                this.emailValidator = true;
+            } else {
+                this.emailValidator = false;
+            }
+        }
 
+    }
+    isValid() {
+        if (this.firstNameValidator === true &&
+            this.lastNameValidator === true &&
+            this.emailValidator === true &&
+            this.userNameValidator === true &&
+            this.passwordValidator === true) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
 }
@@ -49,41 +75,78 @@ const lastName = document.getElementById('lname');
 const userName = document.getElementById('username');
 const password = document.getElementById('password');
 const email = document.getElementById('email');
-const message = document.getElementById('message');
+const errorBox = document.querySelector('.error-message');
+const form = document.querySelector('form');
+const submitBtn = document.querySelector('button');
 
 const errorColor = '#FF6B6B';
 
-const formHandler = new FormHandler(firstName, lastName, userName, password, email, message);
+const formHandler = new FormHandler(firstName, lastName, userName, password, email);
 
 
 firstName.addEventListener('input', (e) => {
     formHandler.validateName(e);
-    if(!formHandler.firstNameValidator) {
+    if (!formHandler.firstNameValidator) {
         firstName.style.color = errorColor;
+
     }
-    if(formHandler.firstNameValidator) {
+    if (formHandler.firstNameValidator) {
         firstName.style.color = 'var(--text)';
     }
 })
 
 lastName.addEventListener('input', (e) => {
     formHandler.validateName(e);
-    if(!formHandler.lastNameValidator) {
+    if (!formHandler.lastNameValidator) {
         lastName.style.color = errorColor;
     }
-    if(formHandler.lastNameValidator) {
+    if (formHandler.lastNameValidator) {
         lastName.style.color = 'var(--text)';
     }
 })
 
 userName.addEventListener('input', (e) => {
     formHandler.validateName(e);
-    if(!formHandler.userNameValidator) {
+    if (!formHandler.userNameValidator) {
         userName.style.color = errorColor;
     }
-    if(formHandler.userNameValidator) {
+    if (formHandler.userNameValidator) {
         userName.style.color = 'var(--text)';
     }
+    // formHandler.userNameValidator ? userName.style.color = 'var(--text)' : userName.style.color = errorColor;
+})
+
+password.addEventListener('input', (e) => {
+    formHandler.validateName(e);
+    if (!formHandler.passwordValidator) {
+        password.style.color = errorColor;
+    }
+    if (formHandler.passwordValidator) {
+        password.style.color = 'var(--text)';
+    }
+})
+
+email.addEventListener('input', (e) => {
+    formHandler.validateName(e);
+    if (!formHandler.emailValidator) {
+        email.style.color = errorColor;
+    }
+    if (formHandler.emailValidator) {
+        email.style.color = 'var(--text)';
+    }
+})
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    if (formHandler.isValid()) {
+        errorBox.textContent = 'Creating account';
+        form.style.display = 'none';
+    } else {
+        errorBox.textContent = 'There is an error';
+    }
+
+console.log('clicked') 
+
 })
 
 // firstName.addEventListener('input', () => {
