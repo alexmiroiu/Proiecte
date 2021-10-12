3c2b1f8ec4bc417476b7380279c68e5fc7a1b237
alexandru miroiu <alexandrumiroiu@gmail.com>
2021-10-12 09:44:02 +0300

    am restructurat modul in care uploadez imaginea ; am adaugat functionalitate pentru a uploada informatiile pe firebase

--- ceMancamApp/cemancam/src/components/newFoodForm/AddFoodForm.js
+++ ceMancamApp/cemancam/src/components/newFoodForm/AddFoodForm.js
@@ -1,11 +1,12 @@
 import { Fragment, useState } from "react"
-import ImageUpload from "./ImageUpload";
 import styles from './AddFoodForm.module.css';
 
 const AddFoodForm = () => {
     const [foodName, setFoodName] = useState('');
     const [time, setTime] = useState('');
     const [recipe, setRecipe] = useState('');
+    const [newImage, setNewImage] = useState();
+    const [imgUrl, setImgUrl] = useState("");
 
     const getFoodName = (event) => {
         setFoodName(event.target.value);
@@ -18,20 +19,46 @@ const AddFoodForm = () => {
         setRecipe(event.target.value);
     }
 
-    const getImg = (url) => {
-        const foodItem = {
-            name: foodName,
-            time: time,
-            recipe: recipe,
-            img: url
-        }
-        console.log(foodItem)
+    const getImage = event => {
+        setNewImage(event.target.files[0]);
     }
 
-    
-
-    const sendData = (event) => {
+    const uploadImage = async (event) => {
         event.preventDefault();
+        const data = new FormData();
+        data.append('file', newImage);
+        data.append('upload_preset', 'axmimages');
+
+        const request = await fetch('https://api.cloudinary.com/v1_1/axmwebsitesro/image/upload', {
+            method: 'POST',
+            body: data
+        })
+        const response = await request.json();
+        console.log(response);
+        setImgUrl(response.secure_url)
+        console.log(imgUrl)
+    }
+    
+    const foodItem = {
+        name: foodName,
+        time: time,
+        recipe: recipe,
+        img: imgUrl
+    }
+
+    const storeRecipe = async (event) => {
+        event.preventDefault();
+        const request = await fetch('https://cemancam-14798-default-rtdb.europe-west1.firebasedatabase.app/recipes.json', {
+            method: 'POST',
+            body: JSON.stringify(foodItem),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+        const response = await request.json();
+        console.log(response);
+
+
     }
 
     
@@ -45,8 +72,10 @@ const AddFoodForm = () => {
             <input type="text" id="time" name="time" value={time} onChange={getTime} />
             <label htmlFor="recipe">Ingrediente si reteta</label>
             <textarea type="text" id="recipe" name="recipe" onChange={getRecipe}/>
-            <ImageUpload saveImgUrl={getImg}/>
-            <button onClick={sendData}>Adauga</button>
+            <input type='file'  onChange={getImage}/>
+            <button onClick={uploadImage}>Upload Image</button>
+            {imgUrl && <img src={imgUrl} alt='upload'/>}
+            <button onClick={storeRecipe}>Adauga</button>
         </form>
         </Fragment>
     )
