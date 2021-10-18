9c7629b7f602759e1a94a9a886f9a0c166e21f7e
alexandru miroiu <alexandrumiroiu@gmail.com>
2021-10-18 09:45:26 +0300

    error catching pe fetch requestul care genereaza lista de preparate

--- ceMancamApp/cemancam/src/components/FoodList.js
+++ ceMancamApp/cemancam/src/components/FoodList.js
@@ -1,28 +1,38 @@
-import React, { Fragment, useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import FoodItem from "./FoodItem";
+import styles from "./FoodList.module.css";
 
 const FoodList = () => {
     const [foodList, setFoodList] = useState([]);
     const [selectedType, setSelectedType] = useState('Toate');
-    const [searchPhrase, setSearchPhrase] = useState('')
+    const [searchPhrase, setSearchPhrase] = useState('');
+    const [error, setError] = useState(null);
 
     const getFoods = async () => {
-        const request = await fetch('https://cemancam-14798-default-rtdb.europe-west1.firebasedatabase.app/recipes.json');
-        const response = await request.json();
-        let listOfFoods = [];
-        // console.log(response);
-        for(const item in response) {
-            const food = {
-                id: item, 
-                name: response[item].name,
-                time: response[item].time,
-                type: response[item].type,
-                recipe: response[item].recipe,
-                image: response[item].img
+        try {
+            const request = await fetch('https://cemancam-14798-default-rtdb.europe-west1.firebasedatabase.app/recipes.json');
+            console.log(request);
+            const response = await request.json();
+            if(!response) {
+                throw new Error('Something went wrong!');
             }
-            listOfFoods.push(food);
+            let listOfFoods = [];
+            for(const item in response) {
+                const food = {
+                    id: item, 
+                    name: response[item].name,
+                    time: response[item].time,
+                    type: response[item].type,
+                    recipe: response[item].recipe,
+                    image: response[item].img
+                }
+                listOfFoods.push(food);
+            }
+            setFoodList(listOfFoods);
+
+        } catch(error) {
+            setError(error.message);
         }
-        setFoodList(listOfFoods);
     }
 
     const getSelectedType = (event) => {
@@ -40,7 +50,7 @@ const FoodList = () => {
     },[])
 
     return (
-        <Fragment>
+        <div className={styles.foodList}>
         <label htmlFor="foodType">Selecteaza dupa felul preparatului: </label>
         <select name="foodType" onChange={getSelectedType} value={selectedType} >
                 <option value='Toate'>Toate</option>
@@ -51,10 +61,11 @@ const FoodList = () => {
         </select>
         <label htmlFor="search">Cauta preparatul</label>
         <input type="text" name="search" value={searchPhrase} onChange={search} />
+        {error && <p>{error}</p>}
         {selectedType === 'Toate' && foodList.filter(item => item.name.toLowerCase().includes(searchPhrase.toLowerCase())).map(item => <FoodItem name={item.name} time={item.time} recipe={item.recipe} type={item.type} image={item.image} key={item.id}></FoodItem>)}
         {selectedType && foodList.filter(item => item.type === selectedType && item.name.toLowerCase().includes(searchPhrase.toLowerCase()))
                                  .map(item => <FoodItem name={item.name} time={item.time} recipe={item.recipe} type={item.type} image={item.image} key={item.id}></FoodItem>)}
-        </Fragment>
+        </div>
     );
 }
 
