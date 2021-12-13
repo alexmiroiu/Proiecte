a6222f6f301ccc1e95ab6c640aa2f0352803d5ab
alexandru miroiu <alexandrumiroiu@gmail.com>
2021-12-13 08:37:10 +0200

    hardcoded html quiz questions

--- quiz-app/src/store/index.js
+++ quiz-app/src/store/index.js
@@ -1,8 +1,102 @@
 import { createSlice, configureStore } from '@reduxjs/toolkit';
 
-const quizSlice = createSlice({
+const htmlQuizQuestions = [
+    {
+        questionText: 'What does HTML stands for?',
+        answers: [
+            {answerText: 'Hypertext Machine language.', isCorrect: false},
+            {answerText: 'Hypertext and links markup language.', isCorrect: false},
+            {answerText: 'Hypertext Markup Language.', isCorrect: true},
+            {answerText: 'Hightext machine language.', isCorrect: false}
+        ]
+    },
+    {
+        questionText: 'Which of the following characters indicate closing of a tag?',
+        answers: [
+            {answerText: '.', isCorrect: false},
+            {answerText: '/', isCorrect: true},
+            {answerText: '\\', isCorrect: false},
+            {answerText: '!', isCorrect: false}
+        ]
+    },
+    {
+        questionText: 'What is the font-size of the h1 heading tag?',
+        answers: [
+            {answerText: '3.5 em', isCorrect: false},
+            {answerText: '2.17 em', isCorrect: false},
+            {answerText: '2 em', isCorrect: true},
+            {answerText: '1.5 em', isCorrect: false}
+        ]
+    },
+    {
+        questionText: 'How many heading tags are there in HTML5?',
+        answers: [
+            {answerText: '2', isCorrect: false},
+            {answerText: '3', isCorrect: false},
+            {answerText: '5', isCorrect: false},
+            {answerText: '6', isCorrect: true}
+        ]
+    },
+    {
+        questionText: 'Which of the following attributes is used to add link to any element?',
+        answers: [
+            {answerText: 'link', isCorrect: false},
+            {answerText: 'ref', isCorrect: false},
+            {answerText: 'href', isCorrect: true},
+            {answerText: 'newref', isCorrect: false}
+        ]
+    },
+    {
+        questionText: 'Which of the following tags is used to make a portion of text italic in HTML?',
+        answers: [
+            {answerText: '<italic>', isCorrect: false},
+            {answerText: '<style= “i”>', isCorrect: false},
+            {answerText: '<i>', isCorrect: true},
+            {answerText: '<style=“italic”>', isCorrect: false}
+        ]
+    },
+    {
+        questionText: 'Which of the following attributes is used to open an hyperlink in new tab?',
+        answers: [
+            {answerText: 'tab', isCorrect: false},
+            {answerText: 'href', isCorrect: false},
+            {answerText: 'target', isCorrect: true},
+            {answerText: 'ref', isCorrect: false}
+        ]
+    },
+    {
+        questionText: 'Which of the following tags is used to add a line-break in HTML?',
+        answers: [
+            {answerText: '<br>', isCorrect: false},
+            {answerText: '<break>', isCorrect: false},
+            {answerText: '</break>', isCorrect: false},
+            {answerText: '</br>', isCorrect: true}
+        ]
+    },
+    {
+        questionText: 'Which of the following is the correct way to add background color in HTML?',
+        answers: [
+            {answerText: '<body color = “green”>', isCorrect: false},
+            {answerText: '<body bg-color = “green”>', isCorrect: false},
+            {answerText: '<body style = “background-color=green”>', isCorrect: false},
+            {answerText: '<body style = “background-color: green;”>', isCorrect: true}
+        ]
+    },
+    {
+        questionText: 'Which among the following is correct HTML code for making a checkbox?',
+        answers: [
+            {answerText: '<checkbox>', isCorrect: false},
+            {answerText: '<input type="checkbox">', isCorrect: true},
+            {answerText: '<check>', isCorrect: false},
+            {answerText: '<input type="check">', isCorrect: false}
+        ]
+    }
+
+]
+
+const htmlQuizSlice = createSlice({
     name: 'quiz',
-    initialState: {quizStarted: true},
+    initialState: {questions: htmlQuizQuestions},
     reducers: {
         startQuiz(state) {
             state.quizStarted = true;
@@ -15,9 +109,9 @@ const quizSlice = createSlice({
 
 
 const store = configureStore({
-    reducer: quizSlice.reducer
+    reducer: htmlQuizSlice.reducer
 });
 
-export const quizActions = quizSlice.actions;
+export const htmlQuizActions = htmlQuizSlice.actions;
 
 export default store;
