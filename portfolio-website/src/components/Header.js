bbef5c637e5cab1647e23dc7ff24c36e20042941
alexandru miroiu <alexandrumiroiu@gmail.com>
2021-12-28 18:57:17 +0200

    stilizare header

--- portfolio-website/src/components/Header.js
+++ portfolio-website/src/components/Header.js
@@ -1,9 +1,19 @@
 import classes from './Header.module.css';
+import logo from '../assets/icons/logo.svg';
 
 const Header = () => {
     return <div className={classes.header}>
-        <p>some logo here</p>
-        <nav>some nav items here</nav>
+        <div className={classes.logoHolder}>
+            <img src={logo} alt="logo" />
+        </div>
+        <nav className={classes.mainNav}>
+            <ul className={classes.menuItems}>
+                <li className={classes.navItem}>Despre mine</li>
+                <li className={classes.navItem}>Proiecte</li>
+                <li className={classes.navItem}>contact</li>
+                <li className={classes.navItem}>CV</li>
+            </ul>
+        </nav>
     </div>
 }
 
