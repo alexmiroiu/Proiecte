27e2b858ecf37a2feea3e894788cdb1244d8a09a
alexandru miroiu <alexandrumiroiu@gmail.com>
2021-12-28 13:27:37 +0200

    adaugare footer

--- portfolio-website/src/App.js
+++ portfolio-website/src/App.js
@@ -1,6 +1,7 @@
 import classes from './App.module.css';
 import About from './components/About';
 import Contact from './components/Contact';
+import Footer from './components/Footer';
 import Header from './components/Header';
 import Hero from './components/Hero';
 import Projects from './components/Projects';
@@ -14,6 +15,7 @@ function App() {
     <About />
     <Projects />
     <Contact />
+    <Footer />
 
     </div>
   </main>
--- portfolio-website/src/components/Footer.js
+++ portfolio-website/src/components/Footer.js
@@ -1,10 +1,11 @@
 import { React } from 'react';
 import classes from './Footer.module.css';
+import copyright from '../assets/icons/copyright.svg';
 
 const Footer = () => {
     return <div className={classes.footer}>
-        <img src={} alt='copyright' />
-        <p ></p>
+        <img src={copyright} alt='copyright' />
+        <p>Alex Miroiu 2022</p>
     </div>
 }
 
