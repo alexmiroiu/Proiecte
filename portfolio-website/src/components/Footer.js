47f9647d4cb2ff63cfb4a1084e573b7bc10d8985
alex miroiu <55791919+alexmiroiu@users.noreply.github.com>
2022-01-29 20:22:56 +0200

    modificare footer partea 1

--- portfolio-website/src/components/Footer.js
+++ portfolio-website/src/components/Footer.js
@@ -2,6 +2,10 @@ import { React } from 'react';
 import { useContext } from 'react';
 import classes from './Footer.module.css';
 import SvgCopyright from './iconComponents/Copyright';
+import SvgLocation from './newIconComponents/Location';
+import SvgGithubLogo from './iconComponents/GithubLogo';
+import SvgLinkedin from './iconComponents/Linkedin';
+import SvgEmail from './iconComponents/Email';
 
 import GlobalState from '../store/store';
 
@@ -10,8 +14,24 @@ const Footer = () => {
     const dark = ctx.darkMode;
 
     return <div className={`${classes.footer} ${dark ? classes.footerDark : classes.footerLight}`}>
-        <SvgCopyright className={`${classes.copyright} ${dark ? classes.copyrightDark : classes.copyrightLight}`}/>
+        <div className={classes.footerWrapper}>
+            
+        </div>
+        
+        <div className={classes.contactMeans}>
+            <SvgGithubLogo />
+            <SvgLinkedin />
+            <div className={classes.emailWrapper}>
+                <SvgEmail />
+                <p>alexandrumiroiu@gmail.com</p>
+            </div>
+        </div>
+
         <p>Alexandru Miroiu 2022</p>
+        <div className={classes.location}>
+            <SvgLocation />
+            <p>Prahova, Ploiesti</p>
+        </div>
     </div>
 }
 
