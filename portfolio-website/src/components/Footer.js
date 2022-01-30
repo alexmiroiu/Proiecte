762622e769fb5a38e44a323ccecf74aa19934f0c
alex miroiu <55791919+alexmiroiu@users.noreply.github.com>
2022-01-30 21:22:54 +0200

    update pentru footer, am inclus github link, link catre profilul de linkedin, email-ul si locatia mea

--- portfolio-website/src/components/Footer.js
+++ portfolio-website/src/components/Footer.js
@@ -13,25 +13,31 @@ const Footer = () => {
     const ctx = useContext(GlobalState);
     const dark = ctx.darkMode;
 
+    const language = ctx.currentLanguage;
+
+    const descriptionText = {
+        ro: 'Conceput si realizat de Alexandru Miroiu utilizand React JS',
+        eng: 'Designed and built by Alexandru Miroiu using React JS'
+    }
+
     return <div className={`${classes.footer} ${dark ? classes.footerDark : classes.footerLight}`}>
         <div className={classes.footerWrapper}>
-            
-        </div>
-        
-        <div className={classes.contactMeans}>
-            <SvgGithubLogo />
-            <SvgLinkedin />
+            <div className={classes.contactMeans}>
+                <a href='https://github.com/alexmiroiu' target="_blank" rel="noreferrer noopener">
+                    <SvgGithubLogo className={`${classes.footerIcon} ${dark ? classes.iconDarkH : classes.iconLightH}`} />
+                </a>
+                <SvgLinkedin className={`${classes.footerIcon} ${dark ? classes.iconDarkH : classes.iconLightH}`}/>
+            </div>
             <div className={classes.emailWrapper}>
-                <SvgEmail />
+                <SvgEmail className={`${classes.footerIcon} ${dark ? classes.iconDark : classes.iconLight}`}/>
                 <p>alexandrumiroiu@gmail.com</p>
             </div>
+            <div className={classes.location}>
+                <SvgLocation className={`${classes.footerIcon} ${dark ? classes.iconDark : classes.iconLight}`}/>
+                <p>Prahova, Ploiesti</p>
+            </div>
         </div>
-
-        <p>Alexandru Miroiu 2022</p>
-        <div className={classes.location}>
-            <SvgLocation />
-            <p>Prahova, Ploiesti</p>
-        </div>
+        <p className={dark ? classes.pDark : classes.pLight}>{language === 'ro' ? descriptionText.ro : descriptionText.eng}</p>
     </div>
 }
 
--- portfolio-website/src/components/newIconComponents/index.js
+++ portfolio-website/src/components/newIconComponents/index.js
@@ -1,3 +1,1 @@
-export { default as Email } from "./Email";
-export { default as Linkedin } from "./Linkedin";
 export { default as Location } from "./Location";
