ec5fafd077b1366ccc91096f8d6fd2a68c97da91
alex miroiu <55791919+alexmiroiu@users.noreply.github.com>
2022-03-03 00:12:11 +0200

    adaugare functie de creare cont

--- iCookedThis/src/pages/Auth.js
+++ iCookedThis/src/pages/Auth.js
@@ -1,12 +1,26 @@
 
-import { useState } from 'react';
+import { useState, useRef, useEffect } from 'react';
+import { signUp } from '../store/AuthSlice';
 import classes from './Auth.module.css';
+
+const signUpUrl = `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBp9q1OJDyKHB9lXn_DKjmkqPywz6EsVmo`;
+const signInUrl = `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBp9q1OJDyKHB9lXn_DKjmkqPywz6EsVmo`;
 
 const Auth = () => {
     const [hasAccount, setHasAccount] = useState(true);
+    const nameRef = useRef();
+    const emailRef = useRef();
+    const passwordRef = useRef();
+
 
     const switchAuthForm = () => {
         setHasAccount(prevState => !prevState)
+    }
+
+    const signUpHandler = (event) => {
+        event.preventDefault();
+        console.log(emailRef.current.value, passwordRef.current.value)
+        signUp(signUpUrl, emailRef.current.value, passwordRef.current.value);
     }
 
     return  <div className={classes.authWrapper}>
@@ -15,17 +29,17 @@ const Auth = () => {
                 <form className={classes.authForm}>
                     {!hasAccount && <div className={classes.inputWrapper}>
                         <label htmlFor='name' >Nume</label>
-                        <input type="text" placeholder="Numele tau" name='name' />
+                        <input type="text" placeholder="Numele tau" name='name' ref={nameRef}/>
                     </div>}
                     <div className={classes.inputWrapper}>
                         <label htmlFor='email'>Adresa de email</label>
-                        <input type="email" name='email' placeholder='Introdu o adresa de email valida' />
+                        <input type="email" name='email' placeholder='Introdu o adresa de email valida' ref={emailRef} />
                     </div>
                     <div className={classes.inputWrapper}>
                         <label htmlFor='password'>Parola</label>
-                        <input type="password" name='password' />
+                        <input type="password" name='password' ref={passwordRef}/>
                     </div>
-                    {!hasAccount && <button className={classes.sendBtn}>Creeaza cont</button>}
+                    {!hasAccount && <button onClick={signUpHandler} className={classes.sendBtn}>Creeaza cont</button>}
                     {hasAccount && <button className={classes.sendBtn}>Intra in cont</button>}
                 </form>
 
--- iCookedThis/src/store/AuthSlice.js
+++ iCookedThis/src/store/AuthSlice.js
@@ -3,6 +3,8 @@ import { createSlice } from "@reduxjs/toolkit";
 const AuthSlice = createSlice({
     name: 'Auth',
     initialState: {
+        isLoggedIn: false,
+        userId: null,
 
     },
     reducers: {
@@ -11,5 +13,27 @@ const AuthSlice = createSlice({
 });
 
 export const authActions = AuthSlice.actions;
+
+export const signUp = (url, email, password) => {
+    return async dispatch => {
+            console.log('initiated')
+            const request =  await fetch(url, {
+                method: 'POST',
+                body: JSON.stringify({
+                    email,
+                    password,
+                    returnSecureToken: true
+                }),
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            });
+            const response = await request.json();
+            console.log(response);
+
+
+
+    }
+}
 
 export default AuthSlice;
