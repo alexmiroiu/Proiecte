f94b35293cc9900552063e116da406f233a4bb58
alex miroiu <55791919+alexmiroiu@users.noreply.github.com>
2022-07-03 21:55:55 +0300

    setting up hookform practice

---
+++ NextJS-Tests/pages/form-demo/index.tsx
@@ -0,0 +1,15 @@
+import styles from './styles.module.scss';
+
+const FormDemo = () => {
+
+
+  return <div className={styles.main}>
+    <h1>Hello</h1>
+    <form>
+      <h2>Just a form full of imputs</h2>
+      <label></label>
+    </form>
+  </div>
+}
+
+export default FormDemo;
