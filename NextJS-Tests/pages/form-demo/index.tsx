b2d66ebe4aa4ce54c64540e583b841c0097d7d48
alex miroiu <55791919+alexmiroiu@users.noreply.github.com>
2022-07-04 09:49:53 +0300

    added inputs

--- NextJS-Tests/pages/form-demo/index.tsx
+++ NextJS-Tests/pages/form-demo/index.tsx
@@ -1,15 +1,27 @@
-import styles from './styles.module.scss';
+import styles from "./styles.module.scss";
 
 const FormDemo = () => {
-
-
-  return <div className={styles.main}>
-    <h1>Hello</h1>
-    <form>
-      <h2>Just a form full of imputs</h2>
-      <label></label>
-    </form>
-  </div>
-}
+  return (
+    <div className={styles.main}>
+      <h1>Hello</h1>
+      <form>
+        <h2>Just a form full of imputs</h2>
+        <div className={styles.input_container}>
+          <label htmlFor="name">Your Name</label>
+          <input type="text" id="name" name="name" />
+        </div>
+        <div className={styles.input_container}>
+          <label htmlFor="email">Your Email</label>
+          <input type="email" id="email" name="email" />
+        </div>
+        <div className={styles.input_container}>
+          <label htmlFor="sex">You are</label>
+          <input type="radio" id="sex" name="sex" value="male"/>
+          <input type="radio" id="sex" name="sex" value="female"/>
+        </div>
+      </form>
+    </div>
+  );
+};
 
 export default FormDemo;
